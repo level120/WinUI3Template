51f1fdf6482666123591bb119be2458d9224e201
Gusam Park <tnara9573@gmail.com>
2025-05-08 23:55:54 +0900

    Add libraries and default style

--- Atelier/App.xaml.cs
+++ Atelier/App.xaml.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System.Diagnostics;
+
+using Atelier.Pages;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,6 +28,7 @@ public partial class App
     public App()
     {
         AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        UnhandledException += OnUnhandledException;
 
         InitializeComponent();
     }
@@ -29,14 +39,85 @@ public partial class App
     /// <param name="args">Details about the launch request and process.</param>
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
-        _window = new MainWindow();
+        _window = new Window
+        {
+            SystemBackdrop = new MicaBackdrop(),
+            Title = "Atelier",
+        };
+        _window.AppWindow.SetIcon("Assets/app.ico");
+        _window.AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+        _window.AppWindow.TitleBar.ButtonBackgroundColor = Microsoft.UI.Colors.Transparent;
+
+#if DEBUG
+        if (Debugger.IsAttached)
+        {
+            DebugSettings.BindingFailed += OnBindingFailed;
+        }
+#endif
+        // Do not repeat app initialization when the Window already has content,
+        // just ensure that the window is active
+        if (_window.Content is not Frame)
+        {
+            // Create a Frame to act as the navigation context and navigate to the first page
+            _window.Content = new Frame();
+        }
+        
+        if (_window.Content is Frame rootFrame)
+        {
+            rootFrame.NavigationFailed -= OnNavigationFailed;
+            rootFrame.NavigationFailed += OnNavigationFailed;
+            rootFrame.Navigate(typeof(MainShellPage), args.Arguments);
+        }
+
         _window.Activate();
     }
 
-    private void OnUnhandledException(object sender, System.UnhandledExceptionEventArgs e)
+    private static void OnBindingFailed(object sender, BindingFailedEventArgs e)
     {
-        var exceptionMessage = e.ExceptionObject is Exception exception
-            ? exception.Message
-            : e.ExceptionObject?.ToString() ?? "Unknown error";
+        // Ignore the exception from NonExistentProperty in BindingPage.xaml, 
+        // as the sample code intentionally includes a binding failure.
+        if (e.Message.Contains("NonExistentProperty"))
+        {
+            return;
+        }
+
+        throw new Exception($"A debug binding failed: {e.Message}");
+    }
+
+    private static void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+    {
+        throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+    }
+
+    private static void OnUnhandledException(object sender, System.UnhandledExceptionEventArgs e)
+    {
+        OnUnhandledException(e.ExceptionObject as Exception ?? new Exception("Unknown error"));
+    }
+
+    private static void OnUnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    {
+        e.Handled = true;
+
+        OnUnhandledException(e.Exception);
+    }
+
+    private static void OnUnhandledException(Exception exception)
+    {
+        try
+        {
+            // Create the notification.
+            var notification = new AppNotificationBuilder()
+                .AddText("An exception was thrown.")
+                .AddText($"Type: {exception.GetType()}")
+                .AddText($"Message: {exception.Message}\r\nHResult: {exception.HResult}")
+                .BuildNotification();
+
+            // Show the notification
+            AppNotificationManager.Default.Show(notification);
+        }
+        catch (Exception)
+        {
+            // Ignore.
+        }
     }
 }
---
+++ Atelier/DataContexts/MainPageDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Atelier.Pages;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Atelier.DataContexts;
+
+/// <summary>
+/// <see cref="MainShellPage"/> DataContext
+/// </summary>
+public partial class MainPageDataContext : ObservableObject
+{
+    /// <summary>
+    /// Loading state of the page
+    /// </summary>
+    [ObservableProperty]
+    private bool _loadingState;
+}
